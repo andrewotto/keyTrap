2f402a817adeb41e41d9535a510fbbdaa2c53e08
Andrew Otto <ottoa@ainfosec.com>
2020-04-21 19:54:04 -0400

    Added random emotes to F8

--- keyTrap/main.cpp
+++ keyTrap/main.cpp
@@ -2,10 +2,12 @@
 #include <windows.h>
 #include <vector>
 #include <iostream>
+#include <time.h>
 
 //time between keypresses
 #define SLEEP_DUR_MS 100
 #define LAUGH_SLEEP_MS 3000
+#define EMOTE_SLEEP_MS 5000
 
 //im lazy
 #define KK_1  0x31
@@ -19,17 +21,31 @@
 #define KK_9  0x38
 #define KK_C  0x43
 
-std::vector<WORD> vLaugh{ KK_C, KK_C, KK_2 };
+//page 1
+std::vector<WORD> vYes{ KK_C, KK_1 };
+std::vector<WORD> vNo{ KK_C, KK_2 };
 std::vector<WORD> vHelp{ KK_C, KK_3 };
-std::vector<WORD> vThank{ KK_C, KK_C, KK_3 };
-std::vector<WORD> vRespect{ KK_C, KK_C, KK_C, KK_4 };
-std::vector<WORD> vHello{ KK_C, KK_C, KK_C, KK_2 };
 std::vector<WORD> vInsult{ KK_C, KK_4 };
 std::vector<WORD> vIntimidate{ KK_C, KK_5 };
+
+//page 2
+std::vector<WORD> vSorry{ KK_C, KK_C, KK_1 };
+std::vector<WORD> vLaugh{ KK_C, KK_C, KK_2 };
+std::vector<WORD> vThank{ KK_C, KK_C, KK_3 };
+std::vector<WORD> vFriendlies{ KK_C, KK_C, KK_4 };
+std::vector<WORD> vRetreat{ KK_C, KK_C, KK_5 };
+
+//page 3
+std::vector<WORD> vHold{ KK_C, KK_C, KK_C, KK_1 };
+std::vector<WORD> vHello{ KK_C, KK_C, KK_C, KK_2 };
+std::vector<WORD> vFollow{ KK_C, KK_C, KK_C, KK_3 };
+std::vector<WORD> vRespect{ KK_C, KK_C, KK_C, KK_4 };
+std::vector<WORD> vCharge{ KK_C, KK_C, KK_C, KK_5 };
 
 HHOOK hKeyboardHook;
 
 bool m_bLaughLoop;
+bool m_bEmoteLoop;
 
 void ghostInput(std::vector<WORD> v)
 {
@@ -65,7 +81,7 @@ void processKey(DWORD key)
 	switch (key)
 	{
 	case VK_F1:
-		m_bLaughLoop ? printf("F1 Detected. Enabling Laugh Loop \n") : printf("F1 Detected. Disabling Laugh Loop \n");
+		m_bLaughLoop ? printf("F1 Detected. Disabling Laugh Loop \n") : printf("F1 Detected. Enabling Laugh Loop \n");
 		m_bLaughLoop ? m_bLaughLoop = false : m_bLaughLoop = true;		
 		break;
 	case VK_F2:
@@ -93,7 +109,8 @@ void processKey(DWORD key)
 		ghostInput(vIntimidate);
 		break;
 	case VK_F8:
-		printf("F8 Detected. Sending nothing \n");
+		m_bEmoteLoop ? printf("F8 Detected. Disabling Random Emote Loop \n") : printf("F8 Detected. Enabling Random Emote Loop \n");
+		m_bEmoteLoop ? m_bEmoteLoop = false : m_bEmoteLoop = true;
 		break;
 	default:
 		break;
@@ -188,31 +205,124 @@ DWORD WINAPI my_LaughLoop(LPVOID lpParam)
 			ghostInput(vLaugh);
 		}
 	}
+}
 
+void doEmoteInput()
+{
+	//randomly generate a number between 0 and 14
+	srand(time(NULL));
+	int iRandom = rand() % 14;
 
+	switch(iRandom)
+	{
+		case 0:
+			ghostInput(vYes);
+			printf("Yes\n");
+			break;
+		case 1:
+			ghostInput(vNo);
+			printf("No\n");
+			break;
+		case 2:
+			ghostInput(vHelp);
+			printf("Help\n");
+			break;
+		case 3:
+			ghostInput(vInsult);
+			printf("Insult\n");
+			break;
+		case 4:
+			ghostInput(vIntimidate);		
+			printf("Intimidate\n");
+			break;
+		case 5:
+			ghostInput(vSorry);
+			printf("Sorry\n");
+			break;
+		case 6:
+			ghostInput(vLaugh);
+			printf("Laugh\n");
+			break;
+		case 7:
+			ghostInput(vThank);
+			printf("Thank\n");
+			break;
+		case 8:
+			ghostInput(vFriendlies);
+			printf("Friendlies\n");
+			break;
+		case 9:
+			ghostInput(vRetreat);
+			printf("Retreat\n");
+			break;
+		case 10:
+			ghostInput(vHold); 
+			printf("Hold\n");
+			break;
+		case 11:
+			ghostInput(vHello); 
+			printf("Hello\n");
+			break;
+		case 12:
+			ghostInput(vFollow); 
+			printf("Follow\n");
+			break;
+		case 13:
+			ghostInput(vRespect); 
+			printf("Respect\n");
+			break;
+		case 14:
+			ghostInput(vCharge); 
+			printf("Charge\n");
+			break;		
+		default:
+		break;
+	}
+}
+
+DWORD WINAPI my_EmoteLoop(LPVOID lpParam)
+{
+	HINSTANCE hInstance = GetModuleHandle(NULL);
+
+	if (!hInstance) hInstance = LoadLibrary((LPCSTR)lpParam);
+	if (!hInstance) return 1;
+
+	while (true)
+	{
+		Sleep(EMOTE_SLEEP_MS);
+		if (m_bEmoteLoop)
+		{
+			doEmoteInput();
+		}
+	}
 }
 
 int main(int argc, char* argv[])
 {
 	HANDLE hThread;
 	HANDLE hLaughLoop;
+	HANDLE hEmoteLoop;
 
 	DWORD dwThread;
 	DWORD dwLaughThread;
+	DWORD dwEmoteThread;
 
 	m_bLaughLoop = false;
+	m_bEmoteLoop = false;
 
-	printf("Launching Mordhau Macro Program. Hit 'F12' to cancel. \n");
-	printf("F1 Laugh Loop \n");
+	printf("Launching Mordhau Macro Program 2.0. Hit 'F12' to cancel. \n");
+	printf("F1 Toggle Laugh Loop \n");
 	printf("F2 Help \n");
 	printf("F3 Thank You\n");
 	printf("F4 Respect\n");
 	printf("F5 Hello\n");
 	printf("F6 Insult\n");
 	printf("F7 Intimidate\n");
+	printf("F8 Toggle LoopRandom Emote\n");
 
 	hThread = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)my_HotKey, (LPVOID)argv[0], NULL, &dwThread);
 	hLaughLoop = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)my_LaughLoop, (LPVOID)argv[0], NULL, &dwLaughThread);
+	hEmoteLoop = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)my_EmoteLoop, (LPVOID)argv[0], NULL, &dwEmoteThread);
 
 	//ShowWindow(FindWindowA("ConsoleWindowClass", NULL), false);
 	if (hThread) return WaitForSingleObject(hThread, INFINITE);
